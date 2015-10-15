d10f759ba285605c8aad8bafeb3524a702bb2a69
Prakashselvam <prakashselvam1991@gmail.com>
2015-10-15 19:02:27 +0530

    + implemented QRcode

--- app/src/main/java/com/global/analytics/firstsampleapp/MainActivity.java
+++ app/src/main/java/com/global/analytics/firstsampleapp/MainActivity.java
@@ -1,9 +1,17 @@
 package com.global.analytics.firstsampleapp;
 
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+
+import android.graphics.Bitmap;
+import android.graphics.Point;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Display;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.WindowManager;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.Properties;
@@ -21,7 +29,30 @@ public class MainActivity extends AppCompatActivity {
         Properties properties;
         AssetsPropertyReader assetsPropertyReader = new AssetsPropertyReader(this);
         properties = assetsPropertyReader.getPropertiesFromDocs("ViewConfig.properties");
-        helloworld.setText(sharedDataManager.Username+properties.getProperty("Status")+" "+username);
+        helloworld.setText(sharedDataManager.Username);
+        //Find screen size
+        WindowManager manager = (WindowManager) getSystemService(WINDOW_SERVICE);
+        Display display = manager.getDefaultDisplay();
+        Point point = new Point();
+        display.getSize(point);
+        int width = point.x;
+        int height = point.y;
+        int smallerDimension = width < height ? width : height;
+        smallerDimension = smallerDimension * 3/4;
+        //Encode with a QR Code image
+        QRCodeEncoder qrCodeEncoder = new QRCodeEncoder(sharedDataManager.Username,
+                null,
+                Contents.Type.TEXT,
+                BarcodeFormat.QR_CODE.toString(),
+                smallerDimension);
+        try {
+            Bitmap bitmap = qrCodeEncoder.encodeAsBitmap();
+            ImageView myImage = (ImageView) findViewById(R.id.imageView1);
+            myImage.setImageBitmap(bitmap);
+
+        } catch (WriterException e) {
+            e.printStackTrace();
+        }
 
     }
 
