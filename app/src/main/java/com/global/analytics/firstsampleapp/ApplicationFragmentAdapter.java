23ca86fc933dd11bd3ba3e356d86f1421316cc80
Prakashselvam <prakashselvam1991@gmail.com>
2015-10-17 03:36:20 +0530

    + added seven and eight phase

--- app/src/main/java/com/global/analytics/firstsampleapp/ApplicationFragmentAdapter.java
+++ app/src/main/java/com/global/analytics/firstsampleapp/ApplicationFragmentAdapter.java
@@ -10,7 +10,7 @@ import android.support.v4.app.FragmentPagerAdapter;
  */
 public class ApplicationFragmentAdapter extends FragmentPagerAdapter {
 
-    final int PAGE_COUNT = 6;
+    final int PAGE_COUNT = 8;
     /** Constructor of the class */
     public ApplicationFragmentAdapter(FragmentManager fm) {
         super(fm);
@@ -56,6 +56,20 @@ public class ApplicationFragmentAdapter extends FragmentPagerAdapter {
         }
         else if (arg0 == 5) {
             PhaseSixFragment myFragment = new PhaseSixFragment();
+            Bundle data = new Bundle();
+            data.putInt("current_page", arg0 + 1);
+            myFragment.setArguments(data);
+            return myFragment;
+        }
+        else if (arg0 == 6) {
+            PhaseSevenFragment myFragment = new PhaseSevenFragment();
+            Bundle data = new Bundle();
+            data.putInt("current_page", arg0 + 1);
+            myFragment.setArguments(data);
+            return myFragment;
+        }
+        else if (arg0 == 7) {
+            PhaseEightFragment myFragment = new PhaseEightFragment();
             Bundle data = new Bundle();
             data.putInt("current_page", arg0 + 1);
             myFragment.setArguments(data);
---
+++ app/src/main/java/com/global/analytics/firstsampleapp/PhaseEightFragment.java
@@ -0,0 +1,53 @@
+package com.global.analytics.firstsampleapp;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Created by Prakash on 17/10/15.
+ */
+public class PhaseEightFragment   extends Fragment {
+
+    int mCurrentPage;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        /** Getting the arguments to the Bundle object */
+        Bundle data = getArguments();
+
+        /** Getting integer data of the key current_page from the bundle */
+        mCurrentPage = data.getInt("current_page", 0);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View v = inflater.inflate(R.layout.content_eight_fragment, container,false);
+
+        NiceSpinner Debitcardtype  = (NiceSpinner) v.findViewById(R.id.Debitcardtype);
+        List<String> dataset = new LinkedList<>(Arrays.asList("Debit card type",
+                "VISA Debit", "MASTER Debit"));
+        Debitcardtype.attachDataSource(dataset);
+
+
+        if (mCurrentPage == 1) {
+
+        }else if (mCurrentPage == 2) {
+
+        }else if (mCurrentPage == 3) {
+
+        }
+        return v;
+    }
+
+}
