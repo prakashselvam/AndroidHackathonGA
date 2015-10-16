e553b04fd25499699600f42259a444ca19137283
Prakashselvam <prakashselvam1991@gmail.com>
2015-10-17 03:17:35 +0530

    + implemented card collection

--- app/src/main/java/com/global/analytics/firstsampleapp/ApplicationActivity.java
+++ app/src/main/java/com/global/analytics/firstsampleapp/ApplicationActivity.java
@@ -1,22 +1,27 @@
 package com.global.analytics.firstsampleapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MotionEvent;
+import android.view.View;
+
+import io.card.payment.CardIOActivity;
+import io.card.payment.CreditCard;
 
 /**
  * Created by Prakash on 16/10/15.
  */
 public class ApplicationActivity extends AppCompatActivity {
-
+    private ViewPager pager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_application);
         /** Getting a reference to the ViewPager defined the layout file */
-        ViewPager pager = (ViewPager) findViewById(R.id.pager);
+        pager = (ViewPager) findViewById(R.id.pager);
         /** Getting fragment manager */
         FragmentManager fm = getSupportFragmentManager();
 
@@ -26,6 +31,56 @@ public class ApplicationActivity extends AppCompatActivity {
         /** Setting the pagerAdapter to the pager object */
         pager.setAdapter(pagerAdapter);
     }
+    public void gotoCardActivity(View view){
+        Intent scanIntent = new Intent(this, CardIOActivity.class);
 
+        // customize these values to suit your needs.
+        scanIntent.putExtra(CardIOActivity.EXTRA_REQUIRE_EXPIRY, true); // default: false
+        scanIntent.putExtra(CardIOActivity.EXTRA_REQUIRE_CVV, true); // default: false
+        scanIntent.putExtra(CardIOActivity.EXTRA_REQUIRE_POSTAL_CODE, false); // default: false
 
+        // MY_SCAN_REQUEST_CODE is arbitrary and is only used within this activity.
+        startActivityForResult(scanIntent, 123);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 123) {
+            String resultDisplayStr;
+            boolean success1 = false,success2 = false,success3 = false;
+            if (data != null && data.hasExtra(CardIOActivity.EXTRA_SCAN_RESULT)) {
+                CreditCard scanResult = data.getParcelableExtra(CardIOActivity.EXTRA_SCAN_RESULT);
+
+                // Never log a raw card number. Avoid displaying it, but if necessary use getFormattedCardNumber()
+                resultDisplayStr = "Card Number: " + scanResult.getRedactedCardNumber() + "\n";
+
+                // Do something with the raw number, e.g.:
+                // myService.setCardNumber( scanResult.cardNumber );
+
+                if (scanResult.isExpiryValid()) {
+                    resultDisplayStr += "Expiration Date: " + scanResult.expiryMonth + "/" + scanResult.expiryYear + "\n";
+                    success1 = true;
+                }
+
+                if (scanResult.cvv != null) {
+                    // Never log or display a CVV
+                    resultDisplayStr += "CVV has " + scanResult.cvv.length() + " digits.\n";
+                    success2 = true;
+                }
+
+                if (scanResult.postalCode != null) {
+                    resultDisplayStr += "Postal Code: " + scanResult.postalCode + "\n";
+                    success3 = true;
+                }
+                if (success1 && success2 && success3) pager.setCurrentItem(6,true);
+            }
+            else {
+                resultDisplayStr = "Scan was canceled.";
+            }
+            // do something with resultDisplayStr, maybe display it in a textView
+            // resultTextView.setText(resultStr);
+        }
+        // else handle other activity results
+    }
 }
